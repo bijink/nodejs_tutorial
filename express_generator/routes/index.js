921c541c5c06e5e5d4915a1068d279806b7c0c55
Bijin Kandengala <kbijin528@gmail.com>
2024-03-05 20:23:18 +0530

    part10

--- express_generator/routes/index.js
+++ express_generator/routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Tutorial', name:'Bijin K' });
+router.get('/', function (req, res, next) {
+  const values = ['one', 'two', 'three', 'four']
+  const person = { name: 'Bijin K', comments: { comment: 'This is a sample comment', date: 'Mar 05, 2024' } }
+  const user = {name: 'Tom', admin: false}
+
+  res.render('index', {values, person, user});
 });
 
 module.exports = router;
